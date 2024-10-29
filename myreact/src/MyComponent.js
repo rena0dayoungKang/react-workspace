ca6c30532cffe4aa7b7e942b88cd03fbc673a93a
rena <dayoungrenakang@gmail.com>
2024-10-29 09:52:07 +0900

    Update MyComponent.js

--- myreact/src/MyComponent.js
+++ myreact/src/MyComponent.js
@@ -2,14 +2,14 @@ import React, {Component} from 'react';
 //리액트 컴포넌트는 HTML, CSS, Data(useState) 로 이루어져 있다. 
 
 // class MyComponent extends Component {
-//     render() { //React의 Component에 있는 메서드 render()
+//     render() {                                       //React의 Component에 있는 메서드 render()
 //         return (
-//             <h2>[This is imported Component ]</h2>
+//             <h2>[This is imported Component ]</h2>   //render()함수에서 return 되는 jsx 
 //         ) 
 //     }
 // }
 
-function MyComponent() {
+function MyComponent() {    //생성자함수
     return (
         <h2>[This is imported Component ]</h2>
     )
